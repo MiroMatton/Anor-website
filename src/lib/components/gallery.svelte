bcb6282fae70273b65c598563d88b5a8b9c9c526
MiroMatton <miro.matton@student.odisee.be>
2025-05-12 22:39:55 +0200

    ✨ image gallery + zoom modal

--- src/lib/components/gallery.svelte
+++ src/lib/components/gallery.svelte
@@ -1,6 +1,6 @@
 <script>
   // import all gallery images
-  const images = import.meta.glob("$lib/images/gallery/*.{jpg,png,gif,jpeg}");
+  const images = import.meta.glob("$lib/gallery/*.{jpg,png,gif,jpeg}");
 
   // Extract the paths of the images into an array
   let imagePaths = Object.keys(images);
@@ -11,11 +11,32 @@
   const interval = setInterval(() => {
     currentImageIndex = (currentImageIndex + 1) % imagePaths.length;
   }, 5000);
+
+
+  import ImageModal from "$lib/components/imageModal.svelte";
+
+  let isOpen = false;
+  let currentImage = null;
+
+  function openModal(image) {
+    currentImage = image;
+    isOpen = true;
+  }
+  function closeModal() {
+    isOpen = false;
+  }
 </script>
 
 <div id="gallery">
-  <img src={imagePaths[currentImageIndex]} alt="foto's anor" class="image" />
+  <img src={imagePaths[currentImageIndex]} alt="foto's anor" class="image thumb" on:click={() => openModal(imagePaths[currentImageIndex])} loading="lazy" />
 </div>
+
+<ImageModal
+  src={currentImage}
+  alt={"foto's gite de la neuve forge"}
+  isOpen={isOpen}
+  closeModal={closeModal}
+/>
 
 <style>
   #gallery {
@@ -25,6 +46,18 @@
       height: 40rem;
       width: 100%;
       border-radius: 30px;
+    }
+  }
+
+  .thumb {
+    width: 150px;
+    height: auto;
+    cursor: pointer;
+    border-radius: 4px;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: scale(1.05);
     }
   }
 </style>
---
+++ src/lib/components/imageModal.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    export let src = '';
+    export let alt = '';
+    export let isOpen = false;
+    export let closeModal = () => {};
+</script>
+
+{#if isOpen}
+    <div class="modal-backdrop" on:click={closeModal}>
+            <img src={src} alt={alt} />
+            <button class="close-button" on:click={closeModal}>×</button>
+    </div>
+{/if}
+
+<style>
+    .modal-backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 1000;
+    }
+
+    img {
+        width: 75%;
+        display: block;
+        border-radius: var(--borderRadius);
+    }
+
+    .close-button {
+        position: absolute;
+        top: 1rem;
+        right: 1rem;
+        background: none;
+        border: none;
+        font-size: 3rem;
+        cursor: pointer;
+        color: var(--green);
+    }
+</style>
